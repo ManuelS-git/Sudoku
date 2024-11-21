ef74e45410a00c321f8f86c982823703754743b7
ManuelS-git <manuel.soares@edu.pucrs.br>
2024-11-21 16:21:10 -0300

    Commit com os comentários no código principal

--- src/main/java/sudoku/SudokuFrame.java
+++ src/main/java/sudoku/SudokuFrame.java
@@ -16,73 +16,88 @@ import javax.swing.SwingUtilities;
 @SuppressWarnings("serial")
 public class SudokuFrame extends JFrame {
 
+	//painéis da interface
 	private JPanel buttonSelectionPanel;
 	private SudokuPanel sPanel;
 	
+	//construtor principal
 	public SudokuFrame() {
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		this.setTitle("Sudoku");
 		this.setMinimumSize(new Dimension(800,600));
 		
+
 		JMenuBar menuBar = new JMenuBar();
-		JMenu file = new JMenu("Game");
-		JMenu newGame = new JMenu("New Game");
-		JMenuItem sixBySixGame = new JMenuItem("6 By 6 Game");
+		JMenu file = new JMenu("Game");//menu principal 
+		JMenu newGame = new JMenu("New Game"); //menu para novos jogos
+
+		//opções com jogos com diferentes modos
+		JMenuItem sixBySixGame = new JMenuItem("6 By 6 Game"); //modo de jogo 6x6
 		sixBySixGame.addActionListener(new NewGameListener(SudokuPuzzleType.SIXBYSIX,30));
-		JMenuItem nineByNineGame = new JMenuItem("9 By 9 Game");
+		JMenuItem nineByNineGame = new JMenuItem("9 By 9 Game");//modo de jogo 9x9
 		nineByNineGame.addActionListener(new NewGameListener(SudokuPuzzleType.NINEBYNINE,26));
-		JMenuItem twelveByTwelveGame = new JMenuItem("12 By 12 Game");
+		JMenuItem twelveByTwelveGame = new JMenuItem("12 By 12 Game");//modo de jogo 12x12
 		twelveByTwelveGame.addActionListener(new NewGameListener(SudokuPuzzleType.TWELVEBYTWELVE,20));
 		
-		/*
-		 * need to include this when solving algorithm is improved
-		 JMenuItem sixteenBySizteenGame = new JMenuItem("16 By 16 Game");
-		sixteenBySizteenGame.addActionListener(new NewGameListener(SudokuPuzzleType.SIXTEENBYSIXTEEN,16));
-		*/
+		//adiciona as opões dos jogos ao menu "New game"
 		newGame.add(sixBySixGame);
 		newGame.add(nineByNineGame);
 		newGame.add(twelveByTwelveGame);
-		//newGame.add(sixteenBySizteenGame);
+		
+
 		file.add(newGame);
-		menuBar.add(file);
+		menuBar.add(file);//adiciona o menu game a barra da janela
 		this.setJMenuBar(menuBar);
 		
+
 		JPanel windowPanel = new JPanel();
-		windowPanel.setLayout(new FlowLayout());
-		windowPanel.setPreferredSize(new Dimension(800,600));
+		windowPanel.setLayout(new FlowLayout());//Define o layout para adicionar os componentes 
+		windowPanel.setPreferredSize(new Dimension(800,600));//tamanho do painel
 		
 		buttonSelectionPanel = new JPanel();
 		buttonSelectionPanel.setPreferredSize(new Dimension(90,500));
 
-		sPanel = new SudokuPanel();
+		sPanel = new SudokuPanel(); //criação do painel do tabuleiro 
 		
-		windowPanel.add(sPanel);
-		windowPanel.add(buttonSelectionPanel);
+		//adiciona o painel de tabuleiro
+		windowPanel.add(sPanel); 
+		windowPanel.add(buttonSelectionPanel); //adiciona o painel dos botôes
+
+
 		this.add(windowPanel);
 		
+		//abre a janlea já rodando o jogo 9x9
 		rebuildInterface(SudokuPuzzleType.NINEBYNINE, 26);
 	}
 	
+	//método para reconstruir a onterface com um jogo novo 
 	public void rebuildInterface(SudokuPuzzleType puzzleType,int fontSize) {
+
+		//gera um novo tabuleiro 
 		SudokuPuzzle generatedPuzzle = new SudokuGenerator().generateRandomSudoku(puzzleType);
+
+		//configura o novo tabuleiro no painel 
 		sPanel.newSudokuPuzzle(generatedPuzzle);
-		sPanel.setFontSize(fontSize);
+		sPanel.setFontSize(fontSize); //tamanho da fonte 
+
+		//remove os botões antigos e gera novos com base no jogo escolhido 
 		buttonSelectionPanel.removeAll();
 		for(String value : generatedPuzzle.getValidValues()) {
 			JButton b = new JButton(value);
 			b.setPreferredSize(new Dimension(40,40));
 			b.addActionListener(sPanel.new NumActionListener());
-			buttonSelectionPanel.add(b);
+			buttonSelectionPanel.add(b); //adiciona o botão ao painel 
 		}
 		sPanel.repaint();
 		buttonSelectionPanel.revalidate();
 		buttonSelectionPanel.repaint();
 	}
 	
+	//classe privada para lidar com o novo modo de jogo escolhido 
 	private class NewGameListener implements ActionListener {
 
-		private SudokuPuzzleType puzzleType;
-		private int fontSize;
+		private SudokuPuzzleType puzzleType; //tipo de tabuleiro 
+		private int fontSize; //tamanho da fonte 
 		
 		public NewGameListener(SudokuPuzzleType puzzleType,int fontSize) {
 			this.puzzleType = puzzleType;
@@ -91,6 +106,7 @@ public class SudokuFrame extends JFrame {
 		
 		@Override
 		public void actionPerformed(ActionEvent e) {
+			//reconsgrtou a interface com o tipo de jogo e tamanho da fonte 
 			rebuildInterface(puzzleType,fontSize);
 		}
 	}
@@ -99,6 +115,7 @@ public class SudokuFrame extends JFrame {
 		SwingUtilities.invokeLater(new Runnable() {
 			@Override
 			public void run() {
+				//cria uma instância da janela prinvipal e a torna visível
 				SudokuFrame frame = new SudokuFrame();
 				frame.setVisible(true);
 			}
